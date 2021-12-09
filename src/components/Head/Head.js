4257bcbd1da114ba5b077b25dd6f2f1ab960d4dd
Schmelzofen <sertac.oezdemir@gmail.com>
2021-12-09 21:29:17 +0100

    Entwurf

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Flex_Components from "./Flex_Components"
+import flexComponents from "./FlexComponents"
 import "./Head.css";
 
 const Head = () => {
@@ -9,10 +9,10 @@ const Head = () => {
             <h1>We Are Glint</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, aliquid! Quae dolore vero optio quasi, quisquam quas dicta delectus corrupti. Unde porro placeat, dolorem nihil aspernatur fugiat qui tempora laboriosam ipsum quaerat dolores odit dolore animi corrupti dicta necessitatibus excepturi sapiente! Esse suscipit earum dolores voluptates ipsum nam explicabo nesciunt. Animi ex cupiditate maiores esse quasi dicta, deleniti nostrum, asperiores officiis nesciunt excepturi expedita nemo eos tempore? Repudiandae maxime dolor fugit earum dolorum fuga dolorem vero commodi eum provident, iusto temporibus quae, cumque voluptatum! Voluptas dignissimos tempore veniam, minima repellendus consectetur omnis maxime atque eveniet quas nemo aliquam recusandae at?</p>
             <article class="flex-container">
-                <Flex_Components/>
-                <Flex_Components/>
-                <Flex_Components/>
-                <Flex_Components/>
+                <flexComponents/>
+                <flexComponents/>
+                <flexComponents/>
+                <flexComponents/>
             </article>
         </header>
     );
